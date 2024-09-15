69c22deee493d70afc287a1ec6dc737ff23242d2
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-09-15 21:21:32 +0530

    Create contructordefault.cpp

---
+++ contructordefault.cpp
@@ -0,0 +1,15 @@
+//sharvari murade
+//23070123088
+#include <iostream>
+using namespace std;
+class Mansi
+{
+    public:
+    Mansi() {
+        cout<<"Hello Mansi"<<endl;
+    }
+};
+int main() {
+    Mansi obj; 
+    return 0;
+}
